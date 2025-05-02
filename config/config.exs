396037dc296586d7dc04200bbac2a3a0e90918f6
Antonin M. <antonin@frixel.fr>
2025-05-02 17:06:37 +0200

    Configure gettext

--- config/config.exs
+++ config/config.exs
@@ -61,6 +61,9 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Set french to default locale in gettext
+config :ig_intranet, IgIntranetWeb.Gettext, default_locale: "fr", allowed_locales: ["en", "fr"]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- lib/ig_intranet/gettext.ex
+++
@@ -1,6 +0,0 @@
-defmodule IgIntranet.Gettext do
-  @moduledoc """
-
-  """
-  use Gettext.Backend, otp_app: :ig_intranet
-end
